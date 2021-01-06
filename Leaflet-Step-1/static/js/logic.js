23257f2ad0f28f9c133f8bfe2289a704bf944458
KP <kristanlpruett@gmail.com>
2021-01-05 21:48:45 -0600

    updatedfiles

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -61,3 +61,28 @@ d3.json(queryUrl, function(d) {
         .addTo(myMap)
     })
 })
+
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+  var div = L.DomUtil.create("div", "info legend");
+  var limits = ["<50","50-100","100-150","200","250+"];
+  var colors = ["#00FF00","#80FF00","#FFFF00","#FF8000","#FF0000"];
+  var labels = [];
+
+  var legendInfo = "<h1>Earthquake Depth</h1>" //+
+    // "<div class=\"labels\">" +
+    //   "<div class=\"min\">" + limits[0] + "</div>" +
+    //   "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+    // "</div>";
+
+  div.innerHTML = legendInfo;
+
+  limits.forEach(function(limit, index) {
+    labels.push("<li style=\"background-color: " + colors[index] + "\">"+limits[index]+"</li>");
+  });
+
+  div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  return div;
+};
+
+legend.addTo(myMap);
